d85d5f15aa2079ddfc67a4b58a70dc3f7b013df8
Sebastian Wicki <gandro@gmx.net>
2017-10-26 22:00:48 +0200

    Integrate bindgen

--- build.rs
+++ build.rs
@@ -1,10 +1,12 @@
 extern crate cc;
+extern crate bindgen;
 
-use std::env::var;
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
     // use cool variety if debug mode enabled
-    let variety = if var("DEBUG").map(|val| val=="true").unwrap_or(false) {
+    let variety = if env::var("DEBUG").map(|val| val=="true").unwrap_or(false) {
         "CONFIG_VAR_COOL"
     } else {
         "CONFIG_VAR_HOT"
@@ -12,10 +14,20 @@ fn main() {
 
     cc::Build::new()
         .file("mps-kit/code/mps.c")
-        .file("src/ffi/glue.c")
+        .file("src/ffi/stub.c")
         .define(variety, None)
         .flag("-std=c11")
         .flag_if_supported("-Wimplicit-fallthrough=2")
         .include("mps-kit/code")
         .compile("librustmps.a");
+
+    let bindings = bindgen::Builder::default()
+        .header("mps-kit/code/mps.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
