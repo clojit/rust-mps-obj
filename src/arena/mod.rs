5fd8c9c721cfcb4757a34768e6ea69443676fbe1
Sebastian Wicki <gandro@gmx.net>
2017-10-29 19:04:14 +0100

    Move VmArena to arena::vm

--- src/arena/mod.rs
+++ src/arena/mod.rs
@@ -1,11 +1,10 @@
 //! Arena implementations
 
 use std::sync::Arc;
-use std::ptr;
 
-use errors::{Error, Result};
-use ffi::{mps_arena_class_vm, mps_arena_committed, mps_arena_create_k, mps_arena_destroy,
-          mps_arena_reserved, mps_arena_t};
+use ffi::{mps_arena_committed, mps_arena_destroy, mps_arena_reserved, mps_arena_t};
+
+pub mod vm;
 
 /// Generic MPS arena interface
 pub trait Arena {
@@ -54,45 +53,7 @@ impl Arena for ArenaRef {
     }
 }
 
-/// An MPS arena backed by virtual memory.
-///
-/// See [the reference](https://www.ravenbrook.com/project/mps/master/manual/html/topic/arena.html#virtual-memory-arenas)
-/// for details.
-pub struct VmArena {
-    inner: ArenaRef,
-}
 
-impl VmArena {
-    /// Creates a new virtual memory arena with the specified initial size
-    pub fn with_capacity(capacity: usize) -> Result<Self> {
-        let args = mps_args! {
-             MPS_KEY_ARENA_SIZE: capacity,
-        };
-
-        let arena = unsafe {
-            let mut arena: mps_arena_t = ptr::null_mut();
-            let res = mps_arena_create_k(&mut arena, mps_arena_class_vm(), args);
-
-            Error::result(res).map(|_| RawArena { arena })
-        }?;
-
-        Ok(VmArena {
-            inner: ArenaRef::new(arena),
-        })
-    }
-}
-
-impl Arena for VmArena {
-    fn as_raw(&self) -> mps_arena_t {
-        self.inner.as_raw()
-    }
-}
-
-impl Into<ArenaRef> for VmArena {
-    fn into(self) -> ArenaRef {
-        self.inner
-    }
-}
 
 /// RAII-handle for a raw arena pointer. Destroys the underlying arena on drop.
 struct RawArena {
---
+++ src/arena/vm.rs
@@ -0,0 +1,47 @@
+//! Virtual memory arena
+
+use std::ptr;
+
+use errors::{Result, Error};
+use ffi::{mps_arena_class_vm, mps_arena_create_k, mps_arena_t};
+use arena::{Arena, ArenaRef, RawArena};
+
+/// An MPS arena backed by virtual memory.
+///
+/// See [the reference](https://www.ravenbrook.com/project/mps/master/manual/html/topic/arena.html#virtual-memory-arenas)
+/// for details.
+pub struct VmArena {
+    inner: ArenaRef,
+}
+
+impl VmArena {
+    /// Creates a new virtual memory arena with the specified initial size
+    pub fn with_capacity(capacity: usize) -> Result<Self> {
+        let args = mps_args! {
+             MPS_KEY_ARENA_SIZE: capacity,
+        };
+
+        let arena = unsafe {
+            let mut arena: mps_arena_t = ptr::null_mut();
+            let res = mps_arena_create_k(&mut arena, mps_arena_class_vm(), args);
+
+            Error::result(res).map(|_| RawArena { arena })
+        }?;
+
+        Ok(VmArena {
+            inner: ArenaRef::new(arena),
+        })
+    }
+}
+
+impl Arena for VmArena {
+    fn as_raw(&self) -> mps_arena_t {
+        self.inner.as_raw()
+    }
+}
+
+impl Into<ArenaRef> for VmArena {
+    fn into(self) -> ArenaRef {
+        self.inner
+    }
+}
